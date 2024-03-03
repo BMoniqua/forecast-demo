28d9949eeba23cfe4d8a08a1b3faa47d28144c93
BMoniqua <mg6752@gmail.com>
2024-03-03 16:01:11 -0500

    api days  updated + functions

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+//function for getting the days
+function formatDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 //adding the API for the real forecast data
 function getForecast(city) {
   let apiKey = "f0c88b3aa014acb2c0t0fo1794b1b032";
@@ -66,24 +74,27 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date"> ${day}</div>
-    <div class= "weather-forecast-icon"> 🌕 </div>
+    <div class="weather-forecast-date"> ${formatDate(day.time)}</div>
+    <div> <img src= "${
+      day.condition.icon_url
+    }" class= "weather-forecast-icon"/> </div>
     <div class="weather-forecast-temperatures">
       <div class= "max">
-      <strong> 15° </strong>
+      <strong> ${Math.round(day.temperature.maximum)}° </strong>
       </div> 
-      <div class= "min"> 9° </div>
+      <div class= "min"> ${Math.round(day.temperature.minimum)}° </div>
     </div>
   </div>  
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
