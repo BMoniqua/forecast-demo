d3539577baedc30edcdf7ca75316b82adb4dd330
BMoniqua <mg6752@gmail.com>
2024-03-04 11:38:01 -0500

    temperature digit in front added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function refreshWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = `0${Math.round(temperature)}`;
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class= "weather-app-icon"/>`;
 
   getForecast(response.data.city);
